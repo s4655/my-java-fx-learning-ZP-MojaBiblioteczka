7679d59800edd44fde206840cc58b5f41949cae4
Boguslaw Przygodzki <bo_adv@interia.pl>
2019-04-07 12:01:35 +0200

    Added TopMenuButtons onAction methods in TopMenuButtonsController

--- src/main/java/pl/moja/biblioteczka/controllers/MainController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -1,5 +1,22 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize() {
+
+        topMenuButtonsController.setMainController(this);
+
+    }
+
 
 }
--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -1,4 +1,28 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class TopMenuButtonsController {
+
+    private MainController mainController;
+
+    @FXML
+    public void openLibrary() {
+        System.out.println("openLibrary");
+    }
+
+    @FXML
+    public void openBookList() {
+        System.out.println("openBookList");
+    }
+
+    @FXML
+    public void openStatistics() {
+        System.out.println("openStatistics");
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 }
