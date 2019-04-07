8590d33f8bfef7a8cfb6e607dcbf5111a551dbaf
Boguslaw Przygodzki <bo_adv@interia.pl>
2019-04-07 23:22:02 +0200

    - Added TopMenuButtons onAction implementation. - Added fxml view for each action. - Pressing button sets corresponding fxml view to the center of BorderPaneMain

--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -2,27 +2,47 @@ package pl.moja.biblioteczka.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    public static final String LIBRARY_FXML_PATH = "/fxml/Library.fxml";
+    public static final String BOOK_LIST_FXML_PATH = "/fxml/BookList.fxml";
+    public static final String STATISTICS_FXML_PATH = "/fxml/Statistics.fxml";
+    public static final String ADD_BOOK_FXML_PATH = "/fxml/AddBook.fxml";
+
     private MainController mainController;
-
-    @FXML
-    public void openLibrary() {
-        System.out.println("openLibrary");
-    }
-
-    @FXML
-    public void openBookList() {
-        System.out.println("openBookList");
-    }
-
-    @FXML
-    public void openStatistics() {
-        System.out.println("openStatistics");
-    }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
+
+    @FXML
+    private ToggleGroup toggleButtons;
+
+    @FXML
+    public void openLibrary() {
+        mainController.setCenter(LIBRARY_FXML_PATH);
+    }
+
+    @FXML
+    public void openBookList() {
+        mainController.setCenter(BOOK_LIST_FXML_PATH);
+    }
+
+    @FXML
+    public void openStatistics() {
+        mainController.setCenter(STATISTICS_FXML_PATH);
+    }
+
+    @FXML
+    public void addBook(ActionEvent actionEvent) {
+        if (toggleButtons.getSelectedToggle() != null) {
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
+
+        mainController.setCenter(ADD_BOOK_FXML_PATH);
+    }
+
+
 }
